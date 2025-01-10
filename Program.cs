c1f96bf6ee3be35fc48fb619be6b0c89053de1f1
Georg Orlov <orlov@altek.online>
2025-01-10 10:24:04 +0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Add1 Add1 = new Add1();
+
+Add1.RegistryAdder1("123");
+
+
+
+
+public class Add1
+{
+    public void RegistryAdder1(string inputString)
+    {
+        Console.WriteLine("Hello, World! " + inputString);
+    }
+}
